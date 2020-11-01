c947d522d6943d40e3a6fc6357345d777853d620
Abdallah Abd-elsabour <38044666+abdallah-abdelsabour@users.noreply.github.com>
2020-11-01 20:40:39 +0200

    Update register.php

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 <html>
 <head>
   <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
   <div class="header">
